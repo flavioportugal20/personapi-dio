e016d7487b64d6289a64af0ff4243e4e78af6cd7
unknown <flavioportugal20@gmail.com>
2021-06-17 11:39:39 -0300

    Improved person creation functionality

--- src/main/java/one/digitalinnovation/personapi/dto/response/MessageResponseDTO.java
+++ src/main/java/one/digitalinnovation/personapi/dto/response/MessageResponseDTO.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.personapi.dto;
+package one.digitalinnovation.personapi.dto.response;
 
 import lombok.Builder;
 import lombok.Data;
@@ -8,5 +8,4 @@ import lombok.Data;
 public class MessageResponseDTO {
 
     private String message;
-
 }
--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -40,4 +40,5 @@ public class Person {
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
+
 }
